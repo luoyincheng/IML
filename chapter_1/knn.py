c0cb4b7971871777860c22ed5113c43231a073f9
luoyincheng <yincheng.luo@gmail.com>
2019-03-28 08:31:00 +0800

    .

--- chapter_1/knn.py
+++ chapter_1/knn.py
@@ -1,6 +1,18 @@
 from numpy import *
 import operator
 
-def classify(inX,dataSet,labels,k):
+
+def classify(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
-    diffMat = tile(inX,(dataSetSize,1)) - dataSet
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+
+
+def createDataSet():
+    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', "A", "B", "B"]
+    return group, labels
+
+if __name__ == '__main__':
+    group,labels = createDataSet()
+    print(group)
+    print(labels)
